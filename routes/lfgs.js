70feff4a0cce9a7cd0d565082587871eaa784132
Endrews <elijah.smiael@gmail.com>
2022-06-28 09:03:51 -0500

    add route for edit button

--- routes/lfgs.js
+++ routes/lfgs.js
@@ -12,6 +12,9 @@ router.get('/new', lfgsCtrl.new)
 // GET -- localhost:3000/lfgs/:id
 router.get('/:id', lfgsCtrl.show)
 
+// GET -- localhost:3000/lfgs/:id/edit
+router.get('/:id/edit', lfgsCtrl.edit)
+
 // POST -- localhost:3000/lfgs
 router.post('/', lfgsCtrl.create)
 
